50279076bc0172b55d9cac219ccea6462011146f
Asier Pereiro <asier.pereiro.castro@cern.ch>
2026-01-11 18:20:17 +0100

    update example

--- main.cc
+++ main.cc
@@ -1,17 +1,19 @@
-#include "dual.h"
+#include "gradient.h"
 #include <fmt/base.h>
 
+using dual_d = Dual<double>;
+dual_d f(const dual_d &x, const dual_d &y, const dual_d &z) {
+  return x * x + y * y + z * z;
+}
+
 int main() {
-  fmt::print("Hello, world!\n");
 
-  Dual<int> x(1, 1), y(1, 1);
+  Vector<double, 3> point(1, 2, 3); // Vector<double, 3>
+  auto grad = gradient(f, point);
 
-  auto z = x + y;
-  fmt::print("z = ({}, {})\n", z.real(), z.dual());
-
-  z = x * y;
-
-  fmt::print("z = ({}, {})\n", z.real(), z.dual());
+  fmt::print("∇f({},{},{}) = ({}, {}, {})\n", point[0], point[1], point[2],
+             grad[0], grad[1], grad[2]);
+  fmt::print("|∇f| = {}\n", grad.norm());
 
   return 0;
 }
