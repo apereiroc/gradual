b0da50dbdf006fa0e8a66fe5adde39f5023395e0
Asier Pereiro <asier.pereiro.castro@cern.ch>
2026-01-12 13:51:38 +0100

    feat: update api

--- main.cc
+++ main.cc
@@ -5,15 +5,13 @@ template <typename T> T f(const T &x, const T &y) { return sin(x) * sin(y); }
 
 int main() {
 
-  Vector init_point{10.0, 20.0};
-
   constexpr double step = 1.0e-3, grad_tol = 1.0e-6;
 
-  Optimiser opt(step, grad_tol);
+  Optimiser<double> opt(step, grad_tol);
 
   fmt::print("Running optimiser... ");
 
-  auto res = opt.run([](auto x, auto y) { return x * x + y * y; }, init_point);
+  auto res = opt.run_from_zero<2>([](auto x, auto y) { return x * x + y * y; });
 
   fmt::print("done\n");
 
